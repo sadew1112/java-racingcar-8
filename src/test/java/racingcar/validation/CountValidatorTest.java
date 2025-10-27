a6490f7b11d1ae39605eebbc9c0afb231d74e2af
[sadew1112] <[bbg380027@gmail.com]>
2025-10-27 20:14:19 +0900

    feat : CountValidator 에 대한 내부 로직 테스트 코드 추가

--- src/test/java/racingcar/validation/CountValidatorTest.java
+++ src/test/java/racingcar/validation/CountValidatorTest.java
@@ -1,5 +1,34 @@
 package racingcar.validation;
 
-public class CountValidatorTest {
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
+class CountValidatorTest {
+
+    private final CountValidator validator = new CountValidator();
+
+    @Test
+    void 정상_케이스() {
+        assertThat(validator.validate("5")).isEqualTo(5);
+    }
+
+    @Test
+    void 입력값_0_예외_케이스() {
+        assertThatThrownBy(() -> validator.validate("0"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 입력값_음수_예외_케이스() {
+        assertThatThrownBy(() -> validator.validate("-1"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 숫자가_아닌_문자면_예외() {
+        assertThatThrownBy(() -> validator.validate("abc2"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
+
